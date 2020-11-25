7c10f42fe1fa7fb74a215d68eb38ec5ac469ac12
nikhil <nikhilrmudholkar29@gmail.com>
2020-11-25 11:06:36 +0530

    backtrader straddle strategy

--- app/day_trading_strategies/banknifty_straddle.py
+++ app/day_trading_strategies/banknifty_straddle.py
@@ -2,11 +2,14 @@ import csv
 import pandas as pd
 import backtrader as bt
 import backtrader.feeds as btfeeds
+import backtrader.analyzers as btanalyzers
+import backtrader.strategies as btstrats
 from os import listdir
 import os
 import itertools
 import json
 from datetime import datetime, timedelta
+import pyfolio as pf
 
 
 class DataIntraday(btfeeds.GenericCSVData):
@@ -34,28 +37,27 @@ class MyStrategy(bt.Strategy):
     market_end = "15:15"
     init_gap = 0
     banknifty_ohlc = pd.read_csv('nifty50_daily_data/combined_scrips_2019.csv')
+    target_hit_count = 0
+    stoploss_hit_count = 0
+    market_end_hit_count = 0
     params = (('market_start', market_start),
               ('market_end', market_end),
               ('init_gap', init_gap),
-              ('nifty50_ohlc_daily', banknifty_ohlc))
+              ('nifty50_ohlc_daily', banknifty_ohlc),
+              ('target_hit_count', target_hit_count),
+              ('stoploss_hit_count', stoploss_hit_count),
+              ('market_end_hit_count', market_end_hit_count))
 
     def log(self, txt, dt=None):
         ''' Logging function for this strategy'''
         dt = dt or (str(self.datas[0].datetime.date(0)) + " " + str(self.datas[0].datetime.time(0)))
         if isinstance(dt, float):
             dt = bt.num2date(dt)
-        # print('%s, %s' % (dt.isoformat(), txt))
         print(dt + " " + str(txt))
 
 
     def notify_order(self, order):
         for index, data in enumerate(self.datas):
-        # if order.status in [order.Submitted, order.Accepted]:
-        #     # Buy/Sell order submitted/accepted to/by broker - Nothing to do
-        #     self.log('ORDER ACCEPTED/SUBMITTED', dt=order.created.dt)
-        #     self.order = order
-        #     return
-            # print(data._name)
             if order.status in [order.Expired]:
                 self.log('BUY EXPIRED')
 
@@ -74,8 +76,6 @@ class MyStrategy(bt.Strategy):
                               order.executed.price,
                               order.executed.value,
                               order.executed.comm))
-
-            # Sentinel to None: new orders allowed
             self.order = None
 
     def notify_trade(self, trade):
@@ -86,36 +86,21 @@ class MyStrategy(bt.Strategy):
 
 
     def __init__(self):
-        # self.intraday_data
         print("inside starategy class")
-
-        # print(market_start)
     def next(self):
-        # for datafeed in self.getdatanames():
         for index, data in enumerate(self.datas):
             datetime_obj = bt.num2date(data.datetime[0])
             time_obj = datetime_obj.time()
             curr_time = str(time_obj.hour)+ ":" +str(time_obj.minute)
             curr_date = str(datetime_obj.date())
             prev_date = str(datetime_obj.date() - timedelta(1))
-            # entry = 0 set entry price on the basis of current outstanding order
-            # print(prev_date)
-            # banknifty_ohlc = pd.read_csv('BANKNIFTY_2019_ohlc.csv')
-             # and  (self.p.nifty50_ohlc_daily['Symbol'] == data._name)
-            # print(self.p.nifty50_ohlc_daily)
             symbol = data._name[:-5]
-            # print(symbol)
             daily_data = self.p.nifty50_ohlc_daily.loc[self.p.nifty50_ohlc_daily['Symbol'] == symbol]
             daily_data = daily_data.reset_index(drop=True)
-            # print((daily_data))
-            # print((daily_data[(daily_data['Date'] == curr_date)].index.values[0]))
-            data_row = daily_data[(daily_data['Date'] == curr_date)].index.values[0].item() - 1
-            # print(type(data_row))
-            # print(daily_data.iloc[data_row, 0])
-            prev_high = daily_data.iloc[data_row, 2]
-            prev_low = daily_data.iloc[data_row, 3]
-            prev_close = daily_data.iloc[data_row, 4]
-            # prev_close = 100
+            prev_data_row = daily_data[(daily_data['Date'] == curr_date)].index.values[0].item() - 1
+            prev_high = daily_data.iloc[prev_data_row, 2]
+            prev_low = daily_data.iloc[prev_data_row, 3]
+            prev_close = daily_data.iloc[prev_data_row, 4]
             gap = (data.open[0] - prev_close)/prev_close
 
             if(curr_time == self.p.market_start):
@@ -124,60 +109,60 @@ class MyStrategy(bt.Strategy):
                     print(str(gap) + "    " + str(4))
                     print("current open = " + str(data.open[0]))
                     print("previous day close = " + str(prev_close))
-
-                    # entry = 100
-                    # self.log('BUY CREATE, %.2f' % self.data.close[0])
-                    self.buy()
+                    self.buy(size = 25)
 
                 if(gap>=0.01):
                     print(str(gap) + "    " + str(5))
-                    # entry = 100
-                    # self.log('sell CREATE, %.2f' % self.data.close[0])
-                    self.sell()
+                    self.sell(size = 25)
 
             if self.position:
-                # print(self.p.init_gap)
                 if(curr_time == self.p.market_end):
-                    # print((self.getposition(self.datas[0].Size)))
                     print(str(gap) + "    " + str(1))
+                    self.p.target_hit_count += 1
                     self.close()
 
                 if (-0.2 * self.p.init_gap <= gap <= 0.2 * self.p.init_gap):
                     print(str(gap) + "    " + str(2))
+                    self.p.stoploss_hit_count += 1
                     self.close()
 
                 if ((gap >= 1.5 * self.p.init_gap) or (gap <= -1.5 * self.p.init_gap)):
                     print(str(gap) + "    " + str(3))
+                    self.p.market_end_hit_count += 1
                     self.close()
-
+    def stop(self):
+        print('number of times target hit = ' + str(self.p.target_hit_count))
+        print('number of times stoploss hit = ' + str(self.p.stoploss_hit_count))
+        print('number of times market end reached = ' + str(self.p.market_end_hit_count))
 
 
 if __name__ == '__main__':
-
-    # data_intraday = DataIntraday(dataname='banknifty_minute_data/BANKNIFTY_2019.csv')
-    cerebro = bt.Cerebro()
-    cerebro.addstrategy(MyStrategy)
-
     data_files = os.listdir('/home/parallax/algo_trading/app/day_trading_strategies/test_minute_data')
     print(data_files)
-    count_break = 0
     for element in data_files:
+        cerebro = bt.Cerebro()
+        cerebro.addstrategy(MyStrategy)
+        cerebro.addanalyzer(btanalyzers.SharpeRatio, timeframe=bt.TimeFrame.Minutes, compression=1, _name='sharpe')
+        cerebro.addanalyzer(bt.analyzers.PyFolio)
+        print(element)
+        # if(element == 'BANKNIFTY_2019.csv'):
+        #     continue
         filename = '/home/parallax/algo_trading/app/day_trading_strategies/test_minute_data/' + element
         data_intraday = DataIntraday(dataname=filename)
         cerebro.adddata(data_intraday)
         print("feed loaded")
-        count_break = count_break + 1
-        if (count_break == 1):
-            break
-
-
-
-
-    # data_daily =
-
-
-
-    cerebro.broker.setcash(100000)
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    cerebro.run()
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        cerebro.broker.setcash(800000)
+        print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        thestrats = cerebro.run()
+        thestrat = thestrats[0]
+        print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+        print('Sharpe Ration = ' , thestrat.analyzers.sharpe.get_analysis())
+        pyfoliozer = thestrat.analyzers.getbyname('pyfolio')
+        returns, positions, transactions, gross_lev = pyfoliozer.get_pf_items()
+        pf.create_full_tear_sheet(
+        returns,
+        positions=positions,
+        transactions=transactions,
+        gross_lev=gross_lev,
+        live_start_date='2020-01-01',  # This date is sample specific
+        round_trips=True)
